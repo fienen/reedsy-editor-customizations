1f6e0a37ccb26d350510f7cb4d0c36a98ad4d4c7
Michael Fienen <fienen@gmail.com>
2025-11-30 10:31:10 -0600

    #11: Remove duplicate div

--- src/removeUpsellPanels.ts
+++ src/removeUpsellPanels.ts
@@ -48,8 +48,7 @@ export const removeUpsellPanels = () => {
                 <span class="vui-icon vui-icon-down" data-v-1a2a55eb data-v-fb827e44>
                     <svg data-v-ae557fc1="" viewBox="0 0 16 16"><g data-v-ae557fc1="" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><path data-v-ae557fc1="" id="icon-fill" d="M8.0000137,9.68417285 L2.7525904,3.59150909 C2.38890834,3.17587246 1.75714573,3.13375495 1.34150909,3.49743701 C0.925872462,3.86111906 0.883754954,4.49288168 1.24743701,4.90851831 L7.24743701,12.4085183 C7.6458483,12.8638455 8.35417911,12.8638455 8.7525904,12.4085183 L14.7525904,4.90851831 C15.1162724,4.49288168 15.0741549,3.86111906 14.6585183,3.49743701 C14.2428817,3.13375495 13.6111191,3.17587246 13.247437,3.59150909 L8.0000137,9.68417285 Z" fill="#2A2A2A" fill-rule="nonzero"></path></g></svg>
                 </span>
-            </button> 
-            <div class="word-count" id="rce-most-used-words"></div>`;
+            </button>`;
             const wordStatWrapper     = document.createElement('div');
             wordStatWrapper.className = 'word-count';
             wordStatWrapper.id        = 'rce-most-used-words';
